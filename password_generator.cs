bb5b45c0e952d6382e8512d4a8d07d5344ab53c5
ivanqqee <goul1488333@gmail.com>
2025-10-03 22:40:29 +0300

    Создан генератор паролей(1)

---
+++ password_generator.cs
@@ -0,0 +1,11 @@
+using System;
+
+class PasswordGenerator
+{
+    static void Main()
+    {
+        Console.WriteLine("Генератор паролей (версия 1.0)");
+        Console.WriteLine("Пароль: abc123");
+    }
+}
+
